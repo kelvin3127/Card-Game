3c5d8c81ad1362fb829e1a0bb2f18da4521289b1
kelvin3127 <c.kelvin3127@gmail.com>
2023-01-05 22:11:45 -0500

    Initial Files
    Added initial Files for React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Card from "./Components/Card/card"
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card/card.js
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+      <div className="Card">
+         <p> This is the fking card</p>
+      </div>
+    );
+  }
+  
+  export default Card;
+  
